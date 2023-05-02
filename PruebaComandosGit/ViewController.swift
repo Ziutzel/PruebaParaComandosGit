6c733e6f7330030c2a198da151ecb90b42c21d96
Oscar Ivan <osivpe@gmail.com>
2023-05-01 21:43:30 -0600

    Se Agrega un boton personalizado

--- PruebaComandosGit/ViewController.swift
+++ PruebaComandosGit/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        print ("hello word")
+      
+        
+        let customButton = UIButton()
+        
+        customButton.setTitle("Mi botón", for: .normal)
+        customButton.backgroundColor = UIColor.blue
+        customButton.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        customButton.center = CGPoint(x: view.center.x, y: view.center.y + 100)
+        customButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        customButton.layer.cornerRadius = 9
+        
+        
+        view.addSubview(customButton)
+
+    }
+    
+    @objc func buttonPressed(_ sender: UIButton) {
+        print("Botón presionado")
     }
 
+
+}
+
+class UnderlinedtextField: UITextField {
+    
+    private let underline = CALayer()
 
 }
 
