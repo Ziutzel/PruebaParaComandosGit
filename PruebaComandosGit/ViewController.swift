d405dbea759da9e95dda824c9f39a93707bf78c9
Ziutzel <ziutzel@hotmail.com>
2023-05-01 19:45:49 -0600

    se añadió un print

---
+++ PruebaComandosGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaComandosGit
+//
+//  Created by Ziutzel grajales on 01/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print ("hello word")
+    }
+
+
+}
+
